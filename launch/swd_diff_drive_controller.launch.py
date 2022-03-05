e83c64ffe4bd36dce98ce01a50f92bdf758d2784
Gregory Mermet <g.mermet@ez-wheel.com>
2022-03-05 02:50:06 -0500

    Add DBus session loading
    Signed-off-by: Gregory Mermet <g.mermet@ez-wheel.com>

--- launch/swd_diff_drive_controller.launch.py
+++ launch/swd_diff_drive_controller.launch.py
@@ -4,38 +4,45 @@
 ## @file swd_diff_drive_controller.launch.py  ##
 ################################################
 
+import sys, os
+import os.path
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
 
-    ld = LaunchDescription()
+    # Load specific dbus user session if exists
+    session = "/tmp/SYSTEMCTL_dbus.id"
+    if os.path.isfile(session):
+        print("SYSTEMCTL_dbus.id detected")
 
-    swd_diff_drive_controller_node = Node(
-        package='swd_ros2_controllers',
-        executable='swd_diff_drive_controller',
-        name='swd_diff_drive_controller',
-        parameters=[
-                    {"baseline_m": 0.485},
-                    {"left_swd_config_file": "/opt/ezw/usr/etc/ezw-smc-core/swd_left_config.ini"},
-                    {"right_swd_config_file": "/opt/ezw/usr/etc/ezw-smc-core/swd_right_config.ini"},
-                    {"pub_freq_hz": 20},
-                    {"watchdog_receive_ms": 500},                    
-                    {"base_frame": "base_link"},
-                    {"odom_frame": "odom"},
-                    {"publish_odom": True},
-                    {"publish_tf": True},
-                    {"publish_safety_functions": True},
-                    {"motor_max_speed_rpm": 1050},
-                    {"motor_max_safety_limited_speed_rpm": 490},
-                    {"have_backward_sls": False},
-                    {"left_encoder_relative_error": 0.2},
-                    {"right_encoder_relative_error": 0.2},
-                    ]
-    )
-
-    ld.add_action(swd_diff_drive_controller_node)
-
-    return ld
+        with open(session) as f:
+            lines = f.readlines()
+        env = dict(line.strip().split("=", 1) for line in lines)
+        os.environ.update(env)
+    
+    return LaunchDescription([
+        Node(
+            package='swd_ros2_controllers',
+            executable='swd_diff_drive_controller',
+            name='swd_diff_drive_controller',
+            parameters=[
+                        {"baseline_m": 0.485},
+                        {"left_swd_config_file": "/opt/ezw/usr/etc/ezw-smc-core/swd_left_config.ini"},
+                        {"right_swd_config_file": "/opt/ezw/usr/etc/ezw-smc-core/swd_right_config.ini"},
+                        {"pub_freq_hz": 20},
+                        {"watchdog_receive_ms": 500},                    
+                        {"base_frame": "base_link"},
+                        {"odom_frame": "odom"},
+                        {"publish_odom": True},
+                        {"publish_tf": True},
+                        {"publish_safety_functions": True},
+                        {"motor_max_speed_rpm": 1050},
+                        {"motor_max_safety_limited_speed_rpm": 490},
+                        {"have_backward_sls": False},
+                        {"left_encoder_relative_error": 0.2},
+                        {"right_encoder_relative_error": 0.2},
+                        ]
+        )
+    ])
